c76f66af38cdd2eea5bd70119eff2e8af2217b5f
razdeep <rrajdeeproychowdhury@gmail.com>
2018-04-23 08:45:25 +0530

    First Commit

---
+++ ArduinoDistanceMeasurer/ArduinoDistanceMeasurer.ino
@@ -0,0 +1,14 @@
+//     Instructions
+// Make the following connections
+// @TODO
+
+#include<LiquidCrystal.h>
+//LiquidCrystal lcd();
+void setup()
+{
+  serial.begin(9600);
+}
+void loop()
+{
+
+}
