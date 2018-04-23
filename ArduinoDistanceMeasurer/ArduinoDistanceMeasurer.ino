51e4ad8071bdb2a103e48b291bd1db1188d82722
razdeep <rrajdeeproychowdhury@gmail.com>
2018-04-23 19:56:36 +0530

    Algorithm comments added

--- ArduinoDistanceMeasurer/ArduinoDistanceMeasurer.ino
+++ ArduinoDistanceMeasurer/ArduinoDistanceMeasurer.ino
@@ -1,14 +1,43 @@
+/*  Copyright (c) Rajdeep Roy Chowdhury 2018 <rrajdeeproychowdhury@gmail.com>
+*   This piece of code is part of my ECE213 Project "Distance Measurer"
+*   of Btech CSE, Sem II (2017-2018)
+*   at Lovely Professional University, Punjab, India
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy of 
+*   this software and associated documentation files (the "Software"), to deal in the Software 
+*   without restriction, including without limitation the rights to use, copy, modify, merge, 
+*   publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+*   to whom the Software is furnished to do so, subject to the following conditions:
+*   The above copyright notice and this permission notice shall be included in all copies or 
+*   substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+*   BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+*   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+*   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 //     Instructions
 // Make the following connections
 // @TODO
 
 #include<LiquidCrystal.h>
-//LiquidCrystal lcd();
+//Init LCD Object
+//Init buzz pin
+//Init pushButton pin
 void setup()
 {
   serial.begin(9600);
+  //Init Serial Monitor
+  //Init LCD with dimensions
+  //Init Pinmode of Buzzer
+  //Init Pinmode of pushButton
+  //displayCredits()
+  //beeps if possible
+  //calibratingText();
 }
 void loop()
 {
-
+  //init unit
+  
 }
