afb91ce876d489fca0d65c1305338efcfeb695bd
razdeep <rrajdeeproychowdhury@gmail.com>
2018-04-26 19:42:42 +0530

    Tiny bug fixed

--- ArduinoDistanceMeasurer/ArduinoDistanceMeasurer.ino
+++ ArduinoDistanceMeasurer/ArduinoDistanceMeasurer.ino
@@ -117,10 +117,7 @@ void loop()
   lcd.print("Distance = ");
   lcd.print(cm);
   lcd.print(" cm");
-  //lcd.clear();
   delay(100);
-  lcd.clear();
-  
 }
 //DEFINITIONS OF CUSTOM FUNCTIONS
 
@@ -162,18 +159,10 @@ void showLoadingDots(int n,int col=0,int row=0){
 }
 long microsecondsToInches(long microseconds)
 {
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
   return microseconds / 74 / 2;
 }
 
 long microsecondsToCentimeters(long microseconds)
 {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
